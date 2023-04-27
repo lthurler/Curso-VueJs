16e5970e95cb8aabc78def4cd661146c60e98bd6
lthurler <leothurler@gmail.com>
2023-04-27 14:06:59 -0300

    fim do projeto

--- 2_projeto/src/views/Pedidos.vue
+++ 2_projeto/src/views/Pedidos.vue
@@ -1,13 +1,44 @@
 <template>
   <section class="pedidos">
-    <h1>PEDIDOS</h1>
+    <h1>Gerenciar pedidos:</h1>
+    <Dashboard />
   </section>
 </template>
 
 <script>
+import Dashboard from '../components/Dashboard.vue'
+
 export default {
-  name:"Pedidos"
+  name: "Pedidos",
+
+  components: {
+    Dashboard
+  }
 }
 
-
 </script>
+
+<!-- {
+      "nome": "landing page",
+      "pao": "3 Queijos",
+      "carne": "Maminha",
+      "opcionais": [
+        "Salame",
+        "Bacon",
+        "Tomate"
+      ],
+      "status": "Solicitado",
+      "id": 1
+    },
+    {
+      "nome": "landing page",
+      "pao": "3 Queijos",
+      "carne": "Maminha",
+      "opcionais": [
+        "Salame",
+        "Bacon",
+        "Tomate"
+      ],
+      "status": "Solicitado",
+      "id": 2
+    } -->
